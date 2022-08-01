62fd2da20e7ad74182d332b7345947e3f0671a5c
Matthieu Dumont <jerska@live.fr>
2022-08-01 20:18:46 +0200

    feat(asdf): add neovim as default package installed

--- asdf/boostrap.sh
+++ asdf/boostrap.sh
@@ -5,4 +5,5 @@ ln -sf $DIR/.asdfrc $HOME/.asdfrc
 ln -sf $DIR/.tool-versions $HOME/.tool-versions
 
 ln -sf $DIR/.default-npm-packages $HOME/.default-npm-packages
+ln -sf $DIR/.default-python-packages $HOME/.default-python-packages
 ln -sf $DIR/.default-gems $HOME/.default-gems
