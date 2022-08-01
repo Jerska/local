0a1f759d4f6ede2b5ea3283dbc346eca8eb19a67
Matthieu Dumont <jerska@live.fr>
2022-08-01 20:21:52 +0200

    fix(vim): update bootstrap

--- vim/bootstrap.sh
+++ vim/bootstrap.sh
@@ -5,4 +5,8 @@ mkdir -p $HOME/.vim
 ln -sf $DIR/.vimrc $HOME/.vimrc
 ln -sf $DIR/.vimrc.plugs $HOME/.vimrc.plugs
 
+mkdir -p ~/.config/nvim
+ln -sf $HOME/.vimrc $HOME/.config/nvim/init.vim
+
 vim +PlugInstall!
+nvim +PlugInstall!
