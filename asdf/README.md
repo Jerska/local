f9064faf06e7f1383350a439996e0de621c149a1
Matthieu Dumont <jerska@live.fr>
2022-08-01 15:41:21 +0200

    feat(asdf): improve

---
+++ asdf/README.md
@@ -0,0 +1,8 @@
+# asdf
+
+# List of installed plugins
+
+```sh
+# Node.js
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+```
--- asdf/boostrap.sh
+++ asdf/boostrap.sh
@@ -3,3 +3,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -sf $DIR/.asdfrc $HOME/.asdfrc
 ln -sf $DIR/.default-npm-packages $HOME/.default-npm-packages
+ln -sf $DIR/.tool-versions $HOME/.tool-versions
