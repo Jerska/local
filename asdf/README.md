f2935269ddd79f9aad8880e5c24629c10955ef63
Matthieu Dumont <jerska@live.fr>
2022-08-01 19:04:15 +0200

    feat(asdf): add ruby

--- asdf/README.md
+++ asdf/README.md
@@ -7,6 +7,7 @@
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin add python https://github.com/danhper/asdf-python
 asdf plugin add golang https://github.com/kennyp/asdf-golang
+asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
 ```
 
 # Notes
--- asdf/boostrap.sh
+++ asdf/boostrap.sh
@@ -2,5 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -sf $DIR/.asdfrc $HOME/.asdfrc
-ln -sf $DIR/.default-npm-packages $HOME/.default-npm-packages
 ln -sf $DIR/.tool-versions $HOME/.tool-versions
+
+ln -sf $DIR/.default-npm-packages $HOME/.default-npm-packages
+ln -sf $DIR/.default-gems $HOME/.default-gems
