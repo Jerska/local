7b0bc237b59534ce28b64496107c93ac06b42ab9
Matthieu Dumont <jerska@live.fr>
2022-08-02 14:56:40 +0200

    feat(asdf): add git suffix to plugins

--- asdf/boostrap.sh
+++ asdf/boostrap.sh
@@ -5,8 +5,8 @@ ln -sf $DIR/.asdfrc $HOME/.asdfrc
 ln -sf $DIR/.tool-versions $HOME/.tool-versions
 
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf plugin add python https://github.com/danhper/asdf-python
-asdf plugin add golang https://github.com/kennyp/asdf-golang
+asdf plugin add python https://github.com/danhper/asdf-python.git
+asdf plugin add golang https://github.com/kennyp/asdf-golang.git
 asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
 
 ln -sf $DIR/.default-npm-packages $HOME/.default-npm-packages
